9546c1c65c6eaad45c0b24e73ac38d1235b5ae1c
Aditya-06 <adityaajmera060401@gmail.com>
2022-03-05 00:17:47 +0530

    Major Bug Fix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,40 @@
+// import express from 'express';
+// // import routes from './routes/route';
+
+// const app = express();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // app.use('/api/user', routes);
+
+// app.get('/api/users/currentUser', (req, res) => {
+//   console.log('API called');
+//   res.send('Hello there!');
+// });
+
+// app.get('/', (req, res) => {
+//   console.log('API called');
+//   res.send('Hello there!');
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}`);
+// });
+
 import express from 'express';
+// import { json } from 'body-parser';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(json());
 
-const PORT = process.env.PORT || 3000;
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!!!!!!');
 });
