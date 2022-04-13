e49fe2d716d20fdcf5a2abb20c14a904d9d069ec
Aditya-06 <adityaajmera060401@gmail.com>
2022-04-13 21:55:58 +0530

    seperate concerns and dev deps

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,50 +1,6 @@
-import express from 'express';
-// import library to overwrite 'next' behaviour of express in async functions
-import 'express-async-errors';
-import cookieSession from 'cookie-session';
-import morgan from 'morgan';
-
-// import routes
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-
-// stuff to do with errors
-import { errorHandler } from './middlewares/error-handlers';
-import { NotFoundError } from './errors/not-found-error';
-
+import { app } from './app';
 // Database configurations
 import { startDb } from './config/db';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-// API logger
-app.use(morgan('tiny'));
-// use all the routes created
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// in case the route hit does not exist
-// in case of async, express relies on next() to handle errors
-app.all('*', async (req, res, next) => {
-  console.log('Not found');
-  throw new NotFoundError();
-});
-
-// error-handler
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
